5470fd2f9ded2abdbb7c4a3bc66abbea268f4944
Orlando L Dominici <orlandoldominici@Orlandos-MacBook-Air-2.local>
2024-08-27 21:12:50 -0400

    adjusted hours for artisan cmd

--- routes/console.php
+++ routes/console.php
@@ -5,7 +5,9 @@ use Illuminate\Support\Facades\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Schedule::command('meter:fetch-popularity')->hourly();
+Schedule::command('meter:fetch-popularity')->hourlyBetween(17, 1)->timezone('America/New_York');
+
+// Schedule::command('meter:fetch-popularity')->hourly();
 
 
 Artisan::command('inspire', function () {
